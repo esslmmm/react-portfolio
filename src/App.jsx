f12626ca8ca31637a7d192564e37d384848ea141
esslmmm <http.esnonofficial@gmail.com>
2024-06-22 14:11:03 +0700

    Add Navbar
    💄feat(Navbar): Logo, Social Media icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 
-import React from 'react'
+import Navbar from "./component/Navbar"
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="overflow-x-hidden text-neutral-300 antialiased slection:bg-cyan-300 selection:text-cyan-900">
+      <div className="flixed top-0 -z-10 h-full w-full"></div>
+      <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
+      <Navbar/>
     </div>
   )
 }
