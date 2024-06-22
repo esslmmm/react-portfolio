3a1509286dc3534f31988b819688db6c4244a45e
esslmmm <http.esnonofficial@gmail.com>
2024-06-22 15:02:46 +0700

    Add Hero Section
    💄feat(Hero Section): Name, Hero Content, Profile

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 
 import Navbar from "./component/Navbar"
+import Hero from "./component/Hero"
 
 const App = () => {
   return (
     <div className="overflow-x-hidden text-neutral-300 antialiased slection:bg-cyan-300 selection:text-cyan-900">
       <div className="flixed top-0 -z-10 h-full w-full"></div>
       <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
+      <div className="container mx-auto px-8">
       <Navbar/>
+      <Hero/>
+      </div>
     </div>
+
   )
 }
 
