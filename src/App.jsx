376d829d834472b01ab09a1daff5a2620c07d441
esslmmm <http.esnonofficial@gmail.com>
2024-06-24 17:30:41 +0700

    Add Motion
    💄feat(Motoin): Hero, About, Technologies, Experience, Project, Contact

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
 
-import Navbar from "./component/Navbar"
-import Hero from "./component/Hero"
-import About from "./component/About"
-import Techologies from "./component/Techologies"
-import Experience from "./component/Experience"
-import Project from "./component/Project"
-import Contact from "./component/Contact"
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+import About from "./components/About"
+import Techologies from "./components/Techologies"
+import Experience from "./components/Experience"
+import Project from "./components/Project"
+import Contact from "./components/Contact"
+import { motion } from "framer-motion"
 
 const App = () => {
   return (
-    <div className="overflow-x-hidden text-neutral-300 antialiased slection:bg-cyan-300 selection:text-cyan-900">
+    <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900">
       <div className="fixed top-0 -z-10 h-full w-full">
-      <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
+      <div class="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
       </div>
+
+
+      
       <div className="container mx-auto px-8">
       <Navbar/>
       <Hero/>
